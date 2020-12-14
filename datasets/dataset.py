8a19340ba86007f91fc40f12517579c2a11a4f3b
Raphael Dias--Monteiro <raphael.dias-monteiro@epita.fr>
2020-12-14 23:49:58 +0100

    datasets: add basic class

---
+++ datasets/dataset.py
@@ -0,0 +1,32 @@
+from torch.utils.data import Dataset, DataLoader
+
+
+class QNetDataset(Dataset):
+
+    def __init__(self, X, Y, Q, first_images, big_images, centers):
+        self.X = X
+        self.Y = Y
+        self.Q = Q
+        self.first_images = first_images
+        self.big_images = big_images
+        self.centers = centers
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, index):
+        X = self.X[index]
+        Y = self.Y[index]
+        Q = self.Q[index]
+        first_image = self.first_images[index]
+        big_image = self.big_images[index]
+        center = self.centers[index]
+
+        return X, Y, Q, first_image, big_image, center
+
+
+class QNetDataLoader(DataLoader):
+
+    def __init__(self, **args, **kwargs):
+        super().__init__(args, kwargs)
+
